29446393961f2b5aed90ee86aee1c44396e2d11a
John Rigler <john@rigler.org>
2026-04-30 09:55:00 -0600

    2.3.10b <--- unspendable enabled in console

--- assets/app.js
+++ assets/app.js
@@ -3,7 +3,7 @@
   // Constants
   //
   const APP_NAME = "chisel";
-  const APP_VERSION = "2.3.9";
+  const APP_VERSION = "2.3.10b";
   const DEFAULT_CURRENCY_KEY = "ravencoin";
   const STATUS_IDLE = "Idle";
   const STATUS_DONE = "Transaction sent successfully.";
