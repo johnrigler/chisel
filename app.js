2d5efd94046d619ee1f965acf96997721ae47288
John Rigler <john@rigler.org>
2026-08-04 20:32:33 -0600

    .

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
   // Constants
   //
   const APP_NAME = "chisel";
-  const APP_VERSION = "2.7.14";
+  const APP_VERSION = "2.7.14a";
   const DEFAULT_CURRENCY_KEY = "litecoin";
   const STATUS_IDLE = "Idle";
   const STATUS_DONE = "Transaction sent successfully.";
